21325d9e6a60434f77c967b965579c1560616ded
yejinleee <yejin9487@gmail.com>
2022-03-03 00:14:31 +0900

    app

--- blog/README.md
+++ blog/README.md
@@ -14,6 +14,7 @@ index.js\
 
 ## JSX
 js에서 html같이 생긴 JSX문법을 return
+return 안에는 크게 하나의 태그가 감싸고 있어야 함.
 
 ## 데이터 바인딩
 서버에서 받아온 데이터를 화면에 보여줄 때 기존 JS의 document.getElement~ 등보다 간편함\
@@ -24,4 +25,23 @@ js에서 html같이 생긴 JSX문법을 return
 style={ obj형 스타일 속성 : "값", }
 _이거또한 {}로 가능_
 
-working?
+## state
+`import React, {useState} from 'react';`
+
+리액트의 데이터 저장공간
+```
+let [변수명, 변경함수] = useStaet("초기값");
+```
+**새로고침 없이** 변경된 내용을 반영할 수 있다 !!
+단, state를 완전히 대체함.
+
+## Event
+``onClick={실행할함수}``\
+``onClcik={ () =>{ 실행할내용}}``
+
+## Component
+리액트의 장점!
+관리 편함.
+반복출현, 자주변경 되는 덩어리를 컴포넌트로 만들어 두면 깔끔해짐
+다른 페이지를 만들때도 유용.
+하지만, state 복잡해짐. 다른 func,컴포넌트에서 선언된 변수 참조 못하니까=>props!!
--- blog/src/App.js
+++ blog/src/App.js
@@ -1,26 +1,52 @@
+import React, {useState} from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title, setTitle] = useState(['제목1','제목2','제목3']);
+  let [thumb, setThumb] = useState(0);
+
+  function changeTitle(){
+    var newArr = [...title];
+    newArr[0]='변경제목1';
+    setTitle(newArr);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style= { {color : 'white', fontSize : '30px'}}> 개발 Blog </div>
+     </div>
+  
+     <button onClick={ changeTitle}>버튼</button>
+     <div className="list">
+      <h3> { title[0] } <span onClick={ ()=> {setThumb(thumb +1 )}}>👍🏻</span> {thumb} </h3>
+      <p>2월 17일 발행</p>
+      <hr/>
+     </div>
+      <div className="list">
+        <h3> { title[1] } </h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { title[2] } </h3>
+        <p>2월 19일 발행</p>
+        <hr/>
+      </div>
+
+
+      <Modal />
     </div>
   );
 }
 
+function Modal(){
+  return(
+    <div>
+      <p>Componnent</p>
+    </div>
+  )
+}
 export default App;
 
