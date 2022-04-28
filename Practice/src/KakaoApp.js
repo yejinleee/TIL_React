09f1184ac65f70b3f5e05fba2574da2940d6d004
yejinleee <yejin9487@gmail.com>
2022-04-29 00:32:29 +0900

    BUG - Route

--- Practice/src/KakaoApp.js
+++ Practice/src/KakaoApp.js
@@ -1,23 +1,25 @@
-import { ConnectedRouter } from 'connected-react-router';
-import { Route, Switch } from 'react-router-dom';
-import history from './history';
+// import { ConnectedRouter } from 'connected-react-router';
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import history from './history';
 // import Home from './pages/Home';
 // import Signin from './pages/Signin';
-import KakaoMapApp from '/KakaoMaps/KakaoMapApp'
+import KakaoMapApp from './KakaoMaps/KakaoMapApp'
 import KakaoRedirectHandler from './KakaoLogin/KakaoRedirectHadler';
 function KakaoApp() {
   return (
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route exact path="/" component={KakaoMapApp} /> 
+    <KakaoMapApp />
+    // <>
+    //   <Routes>
+    //     <Route exact path="/" component={KakaoMapApp} /> 
 
-        {/* <Route exact path="/signin" component={Signin} />*/}
+    //     {/* <Route exact path="/signin" component={Signin} />*/}
 
-        {/* // 아래 부분을 추가해 준것이다. */}
-        <Route path="/oauth/callback/kakao" component={KakaoRedirectHandler} />
-        
-      </Switch>
-    </ConnectedRouter>
+    //     {/* // 아래 부분을 추가해 준것이다. */}
+    //     <Route path="/oauth/callback/kakao" component={KakaoRedirectHandler} />
+    //   </Routes>
+
+    // </>
   );
 }
 
