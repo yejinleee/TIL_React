4caddd3a97dda9d82f84b138559d40828065e1ea
yejinleee <yejin9487@gmail.com>
2022-05-01 00:36:00 +0900

    [Firebase] APP_KEY

--- firebase/src/index.js
+++ firebase/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import firebase from './firebase';
+
+console.log(firebase); //확인용
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
