1f170bb36e4e2efc2749fbf50cf21605b7feedf6
yejinleee <yejin9487@gmail.com>
2022-05-06 22:52:33 +0900

    [Firebase] addDoc

--- firebase/src/routes/Home.js
+++ firebase/src/routes/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
-import { dbService } from "fBase";
-import { addDoc, collection } from "firebase/firestore";
+import { dbService } from "fbase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const Home =()=>{
     const [nweet,setNweet] = useState("");
-    const onSubmit = (e) =>{
+
+    const onSubmit = async(e) =>{
         e.preventDefault();
+        console.log(`서브밋 하는 느윗:${nweet}`);
+        await addDoc(collection(dbService, "nweets"), {
+            nweet,
+            createdAt: serverTimestamp(),
+        });
+        setNweet("");
     }
     const onChange = (e)=>{
         const {target : {value}} = e;
